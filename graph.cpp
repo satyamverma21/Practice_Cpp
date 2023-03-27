8a4fb7cf6d1bdc20c07441cc02adb96338434d1a
SATYAM VERMA <satyamvermabca2021@delhitechnicalcampus.ac.in>
2023-03-28 00:16:40 +0530

    Create graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+using namespace std;
+
+class node
+{
+    int data ;
+    vector<node *> con ;
+
+    node(int data ){
+        this->data=data ;
+    }
+
+    node(int data , int data2)
+    {
+        this->data = data ;
+
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+    return 0;
+}
